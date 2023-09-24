9574a054eacfc74e34abb2c621b303655851517d
nikapadia <30702376+nikapadia@users.noreply.github.com>
2023-09-24 01:37:14 -0400

    Initial canvas setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 // ===== COMPONENTS =====
-import Toolbar from './components/toolbar/toolbar.tsx'
+import Toolbar from './components/toolbar/Toolbar.tsx'
 import ElementsPanel from './components/elements/ElementsPanel.tsx'
 import PropertiesPanel from './components/properties/PropertiesPanel.tsx'
+import Canvas from './components/canvas/Canvas.jsx'
+
+import './App.css';
 
 function App() {
     return (
         <div>
             <Toolbar />
-            <ElementsPanel />
-            <PropertiesPanel />
+            <div className="editor">
+                <ElementsPanel />
+                <Canvas
+                    width={700}
+                    height={500} 
+                />
+                <PropertiesPanel />
+            </div>
         </div>
     );
 }
