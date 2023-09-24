2ed2cb0f017d16cdddbe4de5164fb382b41b6692
nikapadia <30702376+nikapadia@users.noreply.github.com>
2023-09-24 01:04:27 -0400

    Completed adding tailwind

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -6,26 +6,26 @@ import {CodeBlock, TextT, ArrowUpRight, Images } from "@phosphor-icons/react"
 function Toolbar() {
   return (
     <>
-      <div className="toolbar">
-        <div className="toolbar-group toolbar-left">
-          <div className="toolbar-menu-btn toolbar-menu-">
+      <div className="toolbar flex justify-between flex-row absolute box-border bg-core-grey z-10 inset-0 h-12 w-full">
+        <div className="flex justify-start items-center relative h-full basis-1/3">
+          <div className="box-content h-8 w-8 p-2 hover:bg-black">
             <CodeBlock width={32} height={32} color="#fff"/>
           </div>
-          <div className="toolbar-menu-btn">
+          <div className="box-content h-8 w-8 p-2 hover:bg-black">
             <TextT width={32} height={32} color="#fff"/>
           </div>
-          <div className="toolbar-menu-btn">
+          <div className="box-content h-8 w-8 p-2 hover:bg-black">
             <ArrowUpRight width={32} height={32} color="#fff"/>
           </div>
-          <div className="toolbar-menu-btn">
+          <div className="box-content h-8 w-8 p-2 hover:bg-black">
             <Images width={32} height={32} color="#fff"/>
           </div>
         </div>
-        <div className="toolbar-group toolbar-center">Untitled Document</div>
-        <div className="toolbar-group toolbar-right">
-          <button className="toolbar-control-menu btn btn-export"> Export </button>
-          <button className="toolbar-control-menu btn btn-share"> Share </button>
-          <div className="toolbar-control-menu zoom">
+        <div className="flex justify-center items-center h-full basis-2/5">Untitled Document</div>
+        <div className="flex justify-end items-center h-full gap-2 pr-2 basis-1/3">
+          <button className="flex justify-center border-0 p-2 rounded bg-blue-500 hover:rounded-lg"> Export </button>
+          <button className="flex justify-center border-0 p-2 rounded bg-green-400 hover:rounded-lg"> Share </button>
+          <div className="flex items-center h-8 p-2 border-o text-center hover:bg-black box-content">
             <div>100%</div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	mode: "jit",
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"core-grey": "#2C2C2C",
+			},
+		},
+	},
+	plugins: [],
+};
