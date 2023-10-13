29d3a69cf40d801cce497a41bfad9969484ee539
Nilay Kapadia <30702376+nikapadia@users.noreply.github.com>
2023-10-13 16:00:08 -0400

    Fixed conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
 // ===== COMPONENTS =====
-<<<<<<< HEAD
 import Toolbar from './components/toolbar/Toolbar.tsx';
 import ElementsPanel from './components/elements/ElementsPanel.tsx';
 import PropertiesPanel from './components/properties/PropertiesPanel.tsx';
 import Canvas from './components/canvas/Canvas.jsx'
 
 import './app.css';
-=======
-import Toolbar from './components/toolbar/Toolbar.tsx'
-import ElementsPanel from './components/elements/ElementsPanel.tsx'
-import PropertiesPanel from './components/properties/PropertiesPanel.tsx'
->>>>>>> main
 
 function App() {
     return (
