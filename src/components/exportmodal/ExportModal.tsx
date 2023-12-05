96f2d10dec2c36347f45d0832ebe929902c4c0f0
Jason Chen <2kyeti@gmail.com>
2023-12-05 16:44:17 -0500

    PNG Commit
    Download PNG

--- src/components/exportmodal/ExportModal.tsx
+++ src/components/exportmodal/ExportModal.tsx
@@ -7,6 +7,7 @@ interface ExportModalProps {
   layerRef: MutableRefObject<any>;
 }
 
+
 const ExportModal: React.FC<ExportModalProps> = ({ isOpen, onClose, layerRef}) => {
   const [dataUrl, setDataUrl] = React.useState<string | null>(null);
   useEffect(() => {
@@ -22,15 +23,31 @@ const ExportModal: React.FC<ExportModalProps> = ({ isOpen, onClose, layerRef}) =
     event.stopPropagation();
   };
 
+  const handleExport = () => {
+    const dataUrl = layerRef.current?.toDataURL();
+    if (dataUrl) {
+      const link = document.createElement('a');
+      link.download = 'SuperSnippets.png'; // This will prompt the user to save the image as 'canvas.png'
+      link.href = dataUrl;
+      document.body.appendChild(link); // This line is necessary for Firefox
+      link.click();
+      document.body.removeChild(link); // This line is necessary for Firefox
+    }
+  };
+
+
   return (
     <div className="export-modal" onClick={onClose}>
       <div className="modal-content" onClick={stopPropagation}>
-      <button className="close-button" onClick={onClose}>X</button>
-        <h2>Export Data</h2>
+      <div className="modal-flex-container">
         <div className="canvas-container">
         {dataUrl && <img className="canvas-content" src={dataUrl} alt="Canvas Preview" />}
         </div>
-        <button className="export-button">Export to PNG</button>
+          <button className="close-button" onClick={onClose}>X</button>
+          <div className="export-buttons">
+            <button className="export-button" onClick={handleExport}>Export to PNG</button>
+          </div>
+        </div>
       </div>
     </div>
   );
