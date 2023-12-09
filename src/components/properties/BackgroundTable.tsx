c506bc460fac98cee493bc944504cf7f7b7dd805
Thaeriem <kauly.cs@gmail.com>
2023-12-08 21:09:48 -0500

    Update BackgroundTable.tsx

--- src/components/properties/BackgroundTable.tsx
+++ src/components/properties/BackgroundTable.tsx
@@ -1,24 +1,46 @@
-// BackgroundTable.js
 import React from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Input } from '@mui/material';
 import ColorPicker from "./ColorPicker";
 import './backgroundTable.css'; // Import your CSS file
 
+// BackgroundTable component receives properties and handleInputChange as props
 const BackgroundTable = ({ properties, handleInputChange }) => {
+  // Function to get the dimensions of the webpage
+  const getDimensions = () => {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  };
+
+  // Initial dimensions obtained when the component is mounted
+  const initialDimensions = getDimensions();
+
   return (
     <TableContainer component={Paper} className="TableContainer dark-mode">
+      {/* Main Table component */}
       <Table aria-label="properties table">
         <TableHead>
+          {/* Table header row */}
           <TableRow>
             <TableCell align='left'>Background</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
+          {/* Map through properties to render rows */}
           {properties.map((property, index) => (
             <React.Fragment key={index}>
+              {/* Row for ColorPicker component */}
               <TableRow>
-              <TableCell className="colorPickerContainer" colSpan={3}>
+                <TableCell className="colorPickerContainer" colSpan={3}>
                   <ColorPicker />
+                </TableCell>
+              </TableRow>
+              {/* Add a new row for displaying dimensions */}
+              <TableRow>
+                <TableCell align="right" colSpan={2}>
+                  {/* Display the dimensions here */}
+                  {`Dimensions: ${initialDimensions.width}px, ${initialDimensions.height}px`}
                 </TableCell>
               </TableRow>
             </React.Fragment>
